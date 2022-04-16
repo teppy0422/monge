62d91b167077f6f84444a05ac6c3d56b2ee66840
teppy0422 <teppy@aol.jp>
2022-04-17 05:12:03 +0900

    2nd

--- js/custom.js
+++ js/custom.js
@@ -172,3 +172,29 @@ var isHidden = function() {
 };
 //1000ミリ秒後にloadingFunc開始
 setTimeout(isHidden, 100);
+
+// スクロール
+window.addEventListener("load", function() {
+  //プラグインを定義
+  gsap.registerPlugin(ScrollTrigger);
+  const area = document.querySelector(".js-area");
+  const wrap = document.querySelector(".js-wrap");
+  const items = document.querySelectorAll(".js-item");
+  const num = items.length;
+
+  //横幅を指定
+  gsap.set(wrap, { width: num * 100 + "%" });
+  gsap.set(items, { width: 100 / num + "%" });
+
+  gsap.to(items, {
+    xPercent: -100 * (num - 1), //x方向に移動させる
+    ease: "none",
+    scrollTrigger: {
+      trigger: area, //トリガー
+      start: "top top", //開始位置
+      end: "+=1000", //終了位置
+      pin: true, //ピン留め
+      scrub: true, //スクロール量に応じて動かす
+    },
+  });
+});
